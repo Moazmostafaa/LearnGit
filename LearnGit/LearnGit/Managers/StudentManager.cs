9de06abb8146d80f9c20ab7383b98b45ac06b596
Moaz Mostafa <m.mostafa@smartwave.com.eg>
2022-04-13 12:49:54 +0200

    change class

--- LearnGit/LearnGit/Managers/StudentManager.cs
+++ LearnGit/LearnGit/Managers/StudentManager.cs
@@ -8,7 +8,7 @@ namespace LearnGit.Managers
         {
             var student = new Student()
             {
-                Class = "class 6",
+                Class = "class 8",
                 Id = 1,
                 Name = "mohamed",
                 Subjects = new List<int>() { 1, 2, 3, 4, 5, 6 , 7}
