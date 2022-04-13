5bb700668fc244aeefcf17c7a14e5719d3a3a3f9
Moaz Mostafa <m.mostafa@smartwave.com.eg>
2022-04-13 12:52:55 +0200

    chnage id

--- LearnGit/LearnGit/Managers/StudentManager.cs
+++ LearnGit/LearnGit/Managers/StudentManager.cs
@@ -9,7 +9,7 @@ namespace LearnGit.Managers
             var student = new Student()
             {
                 Class = "class 8",
-                Id = 1,
+                Id = 2,
                 Name = "mohamed",
                 Subjects = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 }
             };
