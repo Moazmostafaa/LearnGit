7a8155cd50a0367ea6baa6d104b85ac1b510ef3b
Moaz Mostafa <m.mostafa@smartwave.com.eg>
2022-04-13 12:40:46 +0200

    update student data

--- LearnGit/LearnGit/Managers/StudentManager.cs
+++ LearnGit/LearnGit/Managers/StudentManager.cs
@@ -8,10 +8,10 @@ namespace LearnGit.Managers
         {
             var student = new Student()
             {
-                Class = "class3",
+                Class = "class 6",
                 Id = 1,
-                Name = "John Doe",
-                Subjects = new List<int>() { 1, 2, 3, 4, 5, 6 }
+                Name = "mohamed",
+                Subjects = new List<int>() { 1, 2, 3, 4, 5, 6 , 7}
             };
 
             Console.WriteLine(student);
